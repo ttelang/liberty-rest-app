9e568a46b3eddea887779ac1497c20f475a82cc1
Tarun Telang <tarun.telang@gmail.com>
2025-05-14 16:55:17 +0000

    Remove generated files and configuration for the Liberty REST application
    - Deleted feature.cache, kernel.cache, service.fingerprint, and pom.properties files from the target directory.
    - Removed compiled class files and input files related to HelloWorldApplication and HelloWorldResource.
    - Cleared temporary files including META-INF/MANIFEST.MF.
    - Eliminated server.xml configuration for the MicroProfile tutorial Liberty server.

--- liberty-rest-app/target/liberty/wlp/lib/features/checksums/io.openliberty.activation.internal-2.1.cs
+++
@@ -1,3 +0,0 @@
-#Thu Feb 27 18:49:29 UTC 2025
-lib/features/io.openliberty.activation.internal-2.1.mf=15f90413d593a2a53fde978f276a849f
-lib/io.openliberty.org.eclipse.angus.activation_1.0.98.jar=6e9d715f132e291a6edcd6b9e12b2510
--- liberty-rest-app/target/liberty/wlp/lib/features/checksums/io.openliberty.jakarta.xmlBinding-4.0.cs
+++
@@ -1,4 +0,0 @@
-#Thu Feb 27 18:49:28 UTC 2025
-dev/api/spec/io.openliberty.jakarta.xmlBinding.4.0_1.0.98.jar=d7c3c17b04ca9bdf4aafc64265da9b73
-lib/features/io.openliberty.jakarta.xmlBinding-4.0.mf=e9e706b8bf991766eeb296f0a8ea0652
-lib/io.openliberty.org.glassfish.hk2.osgi-resource-locator_1.0.98.jar=86720dced3eed26a1826a98601cf5786
